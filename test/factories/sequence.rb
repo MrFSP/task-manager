8072ba96a57943a23509a73c9d15c6d01c0ccbe5
Pavel Egorov <epe.m@mail.ru>
2020-07-04 16:01:31 +0300

    Tests added

---
+++ test/factories/sequence.rb
@@ -0,0 +1,18 @@
+FactoryBot.define do
+  sequence :string, aliases: [
+    :first_name,
+    :last_name,
+    :password,
+    :avatar,
+    :name,
+    :description,
+    :state,
+    :expired_at,
+  ] do |n|
+    "string#{n}"
+  end
+
+  sequence :email do |n|
+    "person#{n}@example.com"
+  end
+end
