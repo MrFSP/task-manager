bb66043820df4484427aa4ad495395622c62478b
Pavel Egorov <epe.m@mail.ru>
2020-07-05 12:27:21 +0300

    FSM added to model 'Task'.

--- app/models/task.rb
+++ app/models/task.rb
@@ -5,4 +5,30 @@ class Task < ApplicationRecord
   validates :name, presence: true
   validates :author, presence: true
   validates :description, presence: true, length: { maximum: 500 }
+
+  state_machine initial: :new_task do
+    event :switch_in_development do
+      transition [:new_task, :in_qa, :in_code_review] => :in_development
+    end
+
+    event :switch_in_qa do
+      transition in_development: :in_qa
+    end
+
+    event :switch_code_review do
+      transition in_qa: :in_code_review
+    end
+
+    event :switch_ready_for_release do
+      transition in_code_review: :ready_for_release
+    end
+
+    event :switch_release do
+      transition ready_for_release: :released
+    end
+
+    event :switch_archive do
+      transition [:new_task, :released] => :archived
+    end
+  end
 end
