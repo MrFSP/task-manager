13c997fcf72ca051c3fab17743d0ff579b26dade
Pavel Egorov <epe.m@mail.ru>
2020-07-21 11:11:39 +0300

    Methods added to 'tasks_controller.rb'.

--- app/controllers/api/v1/tasks_controller.rb
+++ app/controllers/api/v1/tasks_controller.rb
@@ -5,12 +5,45 @@ class V1::TasksController < V1::ApplicationController
                 .result
                 .page(page)
                 .per(per_page)
-  
+
     respond_with(
                  tasks,
                  each_serializer: TaskSerializer,
                  root: 'items', 
                  meta: build_meta(tasks)
                 )
+  end
+
+  def create
+    task = current_user.my_tasks.new(task_params)
+    task.save
+
+    respond_with(task, serializer: TaskSerializer, location: nil)
+  end
+
+  def show
+    task = Task.find(params[:id])
+
+    respond_with(task, serializer: TaskSerializer)
+  end
+
+  def update
+    task = Task.find(params[:id])
+    task.update(task_params)
+
+    respond_with(task, serializer: TaskSerializer)
+  end
+
+  def destroy
+    task = Task.find(params[:id])
+    task.destroy
+
+    respond_with(task)
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:name, :description, :author_id, :assignee_id, :state_event)
   end
 end
